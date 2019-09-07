1665e6d87ca08a2f28d42c50e4da9e9ece512920
Akshay <akshay@webllisto.com>
2019-09-07 18:37:15 +0530

    home page like xwish

---
+++ xwish/js/xwish.js
@@ -0,0 +1,11 @@
+jQuery('document').ready(function(){
+
+	jQuery('.single_slide').slick({
+		dots: true,
+		arrows: true,
+		prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fas fa-chevron-left'></i></button>",
+		nextArrow:"<button type='button' class='slick-next pull-right'><i class='fas fa-chevron-right'></i></button>"
+  	});
+				
+
+});
